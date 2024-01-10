5bf8753adac1342399351b61f05e8446d57ce842
zaru <zarutofu@gmail.com>
2024-01-11 07:52:56 +0900

    add headlessUI

--- src/app/sample/[id]/page.tsx
+++ src/app/sample/[id]/page.tsx
@@ -1,14 +1,15 @@
+import {DropDownMenu}  from './DropDownMenu'
 interface Props {
 params: {
   id: string
 }
-
 }
 export default function Page(props: Props) {
   return (
-    <div>
+    <div className="flex gap-4">
       <p>foo</p>
       <p>{props.params.id}</p>
+      <DropDownMenu />
     </div>
   )
 }
